d2cf512d72e225716a1d00f70c97711d709b0ab8
Wassim.rb <rbilawassim@gmail.coml>
2024-04-12 15:30:17 +0200

    frontend update4

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-fetch('https://whatyearisit-backend-ruby-three.vercel.app')
+fetch('https://whatyearisit-backend-ruby-three.vercel.app/year')
 .then(response => response.json())
 .then(data => {document.querySelector('#year').textContent = data.year})
